57e7f27353518b4ad4b3fb6469fbb90261e6caaf
Fabricio Silva da Rocha <fsilvarocha@outlook.com>
2024-06-07 16:25:44 -0300

    Implementação Aplication

---
+++ AutoGlass.Core.Application/DTOs/ProdutoRequest.cs
@@ -0,0 +1,29 @@
+﻿using AutoGlass.Core.Domain.Entities;
+
+namespace AutoGlass.Core.Application.DTOs;
+
+public readonly record struct ProdutoRequest
+{
+    public string Descricao { get; init; }
+    public bool Ativo { get; init; }
+    public DateTime FabricadoEm { get; init; }
+    public DateTime Validade { get; init; }
+    public int IdFornecedor { get; init; }
+    public string DescricaoFornecedor { get; init; }
+    public string CNPJFornecedor { get; init; }
+
+    public static implicit operator Produto(ProdutoRequest request)
+    {
+        return new()
+        {
+            Descricao = request.Descricao,
+            Ativo = request.Ativo,
+            FabricadoEm = request.FabricadoEm,
+            Validade = request.Validade,
+            IdFornecedor = request.IdFornecedor,
+            DescricaoFornecedor = request.DescricaoFornecedor,
+            CNPJFornecedor = request.CNPJFornecedor
+        };
+
+    }
+}
