cb35ada66e97584d4ebc156c0629ce7f28b67f13
Fabricio Silva da Rocha <fsilvarocha@outlook.com>
2024-06-07 16:38:22 -0300

    Implementado infra

---
+++ AutoGlass.Infra.Data/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using AutoGlass.Core.Domain.Entities;
+using AutoGlass.Core.Domain.Notification;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoGlass.Infra.Data.DataContext;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Produto> Produtos { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        ChangeTracker.LazyLoadingEnabled = false;
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<Notification>()
+            .HasNoKey();
+
+
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+    }
+}
