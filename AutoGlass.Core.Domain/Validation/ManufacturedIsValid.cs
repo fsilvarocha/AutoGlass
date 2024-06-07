e3dc913afc922ac37adb6a8fdab71ee7558631ca
Fabricio Silva da Rocha <fsilvarocha@outlook.com>
2024-06-07 16:06:25 -0300

    Implementado Domain

---
+++ AutoGlass.Core.Domain/Validation/ManufacturedIsValid.cs
@@ -0,0 +1,17 @@
+﻿using AutoGlass.Core.Domain.Notification;
+
+namespace AutoGlass.Core.Domain.Validation;
+
+
+public partial class ContractValidations<T>
+{
+    public ContractValidations<T> ValidManufactureIsOk(DateTime Manufacturing, DateTime Validate, string message, string propertyName)
+    {
+        if (Manufacturing >= Validate)
+            AddNotification(new Notification.Notification(message, propertyName));
+
+        return this;
+
+    }
+
+}
